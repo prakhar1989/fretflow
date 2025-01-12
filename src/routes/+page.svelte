1105118374b38f14610719f2fcbab9e08416ed3d
Prakhar Srivastav <prsrivastav@google.com>
2025-01-12 00:04:58 -0800

    add logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
-  import Fretboard from '../lib/components/Fretboard.svelte';
+    import Fretboard from "../lib/components/Fretboard.svelte";
+    import img from "../lib/fretflow.png";
 </script>
 
-<main>
-  <Fretboard></Fretboard>
+<header class="container">
+    <img src={img} alt="logo" />
+    <hgroup>
+        <h1>FretFlow</h1>
+        <p>Guitar Fretboard Visualizer</p>
+    </hgroup>
+</header>
+
+<main class="container">
+    <Fretboard></Fretboard>
 </main>
+
+<style>
+    header {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+    }
+    img {
+        height: 150px;
+    }
+    h1 {
+        font-size: 3em;
+    }
+</style>
