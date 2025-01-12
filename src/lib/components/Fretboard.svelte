d612c66fcc4ec709736584c8c256aff8575621c8
Prakhar Srivastav <prsrivastav@google.com>
2025-01-11 19:21:20 -0800

    add elements to choose frets and key

--- src/lib/components/Fretboard.svelte
+++ src/lib/components/Fretboard.svelte
@@ -1,15 +1,49 @@
 <script lang="ts">
-    import { STANDARD_TUNING, type KEY, NOTES, generateFretboard, generateScaleNotes } from "$lib/music-utils";
+    import {
+        STANDARD_TUNING,
+        type KEY,
+        generateFretboard,
+        generateScaleNotes,
+    } from "$lib/music-utils";
 
-    const NUMBER_OF_FRETS = 24;
+    let numberOfFrets = 24;
+    let key: KEY = "A";
 
-    $: fretboard = generateFretboard(STANDARD_TUNING, NUMBER_OF_FRETS);
-    $: selectedKey = "C" as KEY;
-    $: scaleNotes = generateScaleNotes(selectedKey, "MAJOR");
+    $: fretCount = parseInt(numberOfFrets.toString());
+    $: fretboard = generateFretboard(STANDARD_TUNING, fretCount);
+    $: selectedKey = key;
+    $: scaleNotes = generateScaleNotes(key, "MAJOR");
 </script>
 
+<div class="controls">
+    <div class="frets">
+        <label for="frets">Frets</label>
+        <input type="number" id="frets" bind:value={numberOfFrets} />
+    </div>
+    <div class="keys">
+        <label for="key">Key</label>
+        <select id="key" bind:value={key}>
+            <option value="A">A</option>
+            <option value="Ab">Ab</option>
+            <option value="B">B</option>
+            <option value="Bb">Bb</option>
+            <option value="C">C</option>
+            <option value="C#">C#</option>
+            <option value="D">D</option>
+            <option value="Db">Db</option>
+            <option value="E">E</option>
+            <option value="Eb">Eb</option>
+            <option value="F">F</option>
+            <option value="F#">F#</option>
+            <option value="G">G</option>
+            <option value="G#">G#</option>
+        </select>
+    </div>
+</div>
+
+<!--  FRETBOARD -->
 <div class="numbers">
-    {#each Array.from({ length: NUMBER_OF_FRETS+1 }).map((_, i) => i) as fretIndex}
+    {#each Array.from({ length: fretCount + 1 }).map((_, i) => i) as fretIndex}
         <div class="fret">{fretIndex}</div>
     {/each}
 </div>
@@ -23,9 +57,13 @@
         {#each fretboard as string, stringIndex}
             <div class="string">
                 {#each string as note, fretIndex}
-                    <div class="fret" 
+                    <div
+                        class="fret"
                         class:root={note === selectedKey}
-                        class:highlighted={scaleNotes.has(note)}>{note}</div>
+                        class:highlighted={scaleNotes.has(note)}
+                    >
+                        {note}
+                    </div>
                 {/each}
             </div>
         {/each}
@@ -33,6 +71,14 @@
 </div>
 
 <style>
+    .controls {
+        display: flex;
+        gap: 24px;
+        justify-content: center;
+        align-items: center;
+        margin: 2em 0;
+    }
+
     .fretboard-container {
         display: flex;
         gap: 10px;
@@ -56,7 +102,7 @@
         width: 30px;
         color: #454545;
     }
-    
+
     .tuning .fret {
         color: #ccc;
     }
